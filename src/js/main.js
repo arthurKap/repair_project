4a7e0410c81300a266c1f28dac559e6ad6c5966f
Kapytsia Arthur <marlboro2224@gmail.com>
2021-10-31 16:39:30 +0200

    Пофиксил адаптив блока offer, добавил кнопу scrollTop

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,9 @@
-var button = document.querySelector('#button');
-var modal = document.querySelector('#modal');
-var close = document.querySelector('#close');
+let button = document.querySelector('#button');
+let modal = document.querySelector('#modal');
+let close = document.querySelector('#close');
+
+const toTop = document.querySelector(".scrollTop");
+// Скролл на вверх
 
 button.addEventListener('click', function() {
   modal.classList.add('modal_active');
@@ -10,7 +13,18 @@ close.addEventListener('click', function() {
   modal.classList.remove('modal_active');
 });
 
-setInterval(function () {
+setInterval(function() {
    modal.classList.remove('modal_active');
 }, 5000);
+//Модельное окно
 
+window .addEventListener("scroll", () => {
+  if (window.pageYOffset > 100) {
+    toTop.classList.add("active");
+  }
+  else {
+    toTop.classList.remove("active");
+  }
+
+})
+//Скролл на вверх
